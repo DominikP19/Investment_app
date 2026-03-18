f28dd76f954ce34d446e858ac86b525761129d0c
Dominik PS <dominikpiliszek@gmail.com>
2026-03-18 01:03:00 +0100

    trigger logic added. tax lot and booking still to go

--- db/02_trigger_seed.sql
+++ db/02_trigger_seed.sql
@@ -1,5 +1,5 @@
 --TRIGGER FUNCTIONS
-CREATE OR REPLACE FUNCTION fnc_process_transaction_data() 
+CREATE OR REPLACE FUNCTION fnc_calc_transaction_data() 
 RETURNS TRIGGER
 LANGUAGE plpgsql AS $$
 DECLARE
@@ -19,7 +19,8 @@ BEGIN
 
     NEW.total_amount := (NEW.price * NEW.quantity) + NEW.fee;
 
-    IF TRANSACTION_TYPE_CODE IN ('SEL', 'DIV', 'INT') AND TAX_BRACKET <> 0 THEN
+    -- tax_amount in case of selling calculated and updated based on tax lots
+    IF TRANSACTION_TYPE_CODE IN ('DIV', 'INT') AND TAX_BRACKET <> 0 THEN
         NEW.tax_amount := ROUND((NEW.price * NEW.quantity) * (TAX_BRACKET / 100));
     END IF;
 
@@ -29,7 +30,8 @@ BEGIN
         WHERE portfolio_id = NEW.portfolio_id
         AND asset_id = NEW.asset_id;
 
-        IF NEW.quantity > NVL(POSITION_QUANTITY, 0) THEN
+        IF NEW.quantity > COALESCE(POSITION_QUANTITY, 0) THEN
+        -- @TODO: add logging
             RAISE EXCEPTION 'Trying to sell % when overall holding is %', NEW.quantity, POSITION_QUANTITY;
         END IF;
     END IF;
@@ -60,27 +62,86 @@ BEGIN
 END;
 $$;
 
-CREATE OR REPLACE FUNCTION fnc_update_position()
+CREATE OR REPLACE FUNCTION fnc_calc_position()
 RETURNS TRIGGER
 LANGUAGE plpgsql AS $$
 BEGIN
-    -- @TODO: Implement position update logic
+
+    WITH trans AS (
+        SELECT 
+            sum(CASE WHEN tt.code = 'SEL' THEN (-1) * t.quantity ELSE t.quantity END) AS sum_q, 
+            sum(CASE WHEN tt.code = 'SEL' THEN (-1) * t.total_amount ELSE t.total_amount END) AS sum_t, 
+            sum(t.fee) AS sum_f, 
+            avg(CASE WHEN tt.code = 'BUY' THEN t.price END)::NUMERIC(20,4) AS avg_p, 
+            count(1) AS cnt,
+            NEW.asset_id AS asset_id,
+            NEW.portfolio_id AS portfolio_id,
+            NEW.currency AS currency
+        FROM TRANSACTION t
+        INNER JOIN TRANSACTION_TYPE tt
+        ON t.transaction_type_id = tt.id
+        WHERE 
+            t.portfolio_id = NEW.portfolio_id
+        AND t.asset_id = NEW.asset_id
+        AND t.currency = NEW.currency
+        AND tt.code IN ('BUY', 'SEL')
+    )
+    MERGE INTO POSITION p
+    USING trans t 
+    ON t.asset_id = p.asset_id
+    AND t.portfolio_id = p.portfolio_id
+    WHEN NOT MATCHED THEN
+        INSERT (portfolio_id, asset_id, currency, quantity, average_price, fee, total_amount)
+        VALUES (t.portfolio_id, t.asset_id, t.currency, t.sum_q, t.avg_p, t.sum_f, t.sum_t)
+    WHEN MATCHED AND t.cnt = 1 THEN
+        UPDATE SET 
+            quantity = t.sum_q, 
+            average_price = t.avg_p, 
+            fee = t.sum_f, 
+            total_amount = t.sum_t
+    WHEN MATCHED AND t.cnt > 1 AND t.sum_q = 0 THEN
+        DELETE 
+    WHEN MATCHED AND t.cnt > 1 AND t.sum_q > 0 THEN
+        UPDATE SET 
+            quantity = t.sum_q, 
+            average_price = t.avg_p, 
+            fee = t.sum_f, 
+            total_amount = t.sum_t;
+
+    RETURN NEW;
+END;
+$$;
+
+
+CREATE OR REPLACE FUNCTION fnc_calc_tax_lot()
+RETURNS TRIGGER 
+LANGUAGE plpgsql AS $$
+BEGIN
+    -- @TODO: add tax lot logic here
+    -- update the transaction tax amount in case of selling based on the tax lots
+    -- tax on DIV and INT calculated directly in transaction trigger
     RETURN NEW;
 END;
 $$;
 
 -- TRIGGERS
-CREATE OR REPLACE TRIGGER trg_transaction_calc 
+CREATE OR REPLACE TRIGGER trg_calc_transaction
 BEFORE INSERT OR UPDATE ON TRANSACTION
 FOR EACH ROW
-EXECUTE FUNCTION fnc_process_transaction_data();
+EXECUTE FUNCTION fnc_calc_transaction_data();
 
 CREATE OR REPLACE TRIGGER trg_booking
 AFTER INSERT OR UPDATE ON TRANSACTION
 FOR EACH ROW
 EXECUTE FUNCTION fnc_create_booking();
 
-CREATE OR REPLACE TRIGGER trg_position_update
+CREATE OR REPLACE TRIGGER trg_calc_position
 AFTER INSERT OR UPDATE ON TRANSACTION
 FOR EACH ROW
-EXECUTE FUNCTION fnc_update_position();
+EXECUTE FUNCTION fnc_calc_position();
+
+
+CREATE OR REPLACE TRIGGER trg_calc_tax_lot()
+AFTER INSERT OR UPDATE ON TRANSACTION
+FOR EACH ROW
+EXECUTE FUNCTION fnc_calc_tax_lot();
