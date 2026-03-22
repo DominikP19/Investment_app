43ea6e98d35004178c016c293022ca084201e956
Dominik PS <dominikpiliszek@gmail.com>
2026-03-23 00:43:57 +0100

    trigger + table setup chages

--- db/02_trigger_seed.sql
+++ db/02_trigger_seed.sql
@@ -1,3 +1,21 @@
+CREATE OR REPLACE FUNCTION get_transaction_type(id INTEGER)
+RETURNS transaction_type.code%type
+LANGUAGE SQL 
+BEGIN ATOMIC
+ SELECT CODE FROM TRANSACTION_TYPE WHERE id = get_transaction_type.id;
+END;
+
+CREATE OR REPLACE FUNCTION get_tax_bracket(portfolio_id INTEGER)
+RETURNS tax_rate.rate%type
+LANGUAGE SQL
+BEGIN ATOMIC
+ SELECT RATE 
+    FROM TAX_RATE 
+    WHERE ID = (SELECT tax_rate_id 
+                FROM PORTFOLIO 
+                WHERE id = get_tax_bracket.portfolio_id);
+END;
+
 --TRIGGER FUNCTIONS
 CREATE OR REPLACE FUNCTION fnc_calc_transaction_data() 
 RETURNS TRIGGER
@@ -5,34 +23,60 @@ LANGUAGE plpgsql AS $$
 DECLARE
     TAX_BRACKET tax_rate.rate%type;
     TRANSACTION_TYPE_CODE transaction_type.code%type;
-    POSITION_QUANTITY position.quantity%type;
+    POSITION_QUANTITY transaction.quantity%type;
+    QUANTITY_CNT INTEGER;
+    TAX_BASE_COMP FINANCIAL := 0;
+    lot RECORD;
 BEGIN
-    SELECT rate INTO TAX_BRACKET 
-    FROM TAX_RATE 
-    WHERE ID = (SELECT tax_rate_id 
-                FROM PORTFOLIO 
-                WHERE id = NEW.portfolio_id);
-
-    SELECT code INTO TRANSACTION_TYPE_CODE 
-    FROM TRANSACTION_TYPE 
-    WHERE ID = NEW.transaction_type_id;
+    TAX_BRACKET := get_tax_bracket(NEW.portfolio_id);
+    TRANSACTION_TYPE_CODE := get_transaction_type(NEW.transaction_type_id);
 
     NEW.total_amount := (NEW.price * NEW.quantity) + NEW.fee;
 
     -- tax_amount in case of selling calculated and updated based on tax lots
     IF TRANSACTION_TYPE_CODE IN ('DIV', 'INT') AND TAX_BRACKET <> 0 THEN
-        NEW.tax_amount := ROUND((NEW.price * NEW.quantity) * (TAX_BRACKET / 100));
+        NEW.tax_amount := ROUND((NEW.price * NEW.quantity) * (TAX_BRACKET / 100), 2);
     END IF;
 
     IF TRANSACTION_TYPE_CODE = 'SEL' THEN
-        SELECT quantity INTO POSITION_QUANTITY
-        FROM POSITION 
-        WHERE portfolio_id = NEW.portfolio_id
-        AND asset_id = NEW.asset_id;
+        SELECT 
+        sum(CASE WHEN tt.code = 'SEL' THEN (-1) * t.quantity ELSE t.quantity END) INTO POSITION_QUANTITY
+        FROM TRANSACTION t
+        INNER JOIN TRANSACTION_TYPE tt
+        ON t.transaction_type_id = tt.id
+        WHERE 
+            t.portfolio_id = NEW.portfolio_id
+        AND t.asset_id = NEW.asset_id
+        AND t.currency = NEW.currency
+        AND tt.code IN ('BUY', 'SEL');
 
         IF NEW.quantity > COALESCE(POSITION_QUANTITY, 0) THEN
         -- @TODO: add logging
             RAISE EXCEPTION 'Trying to sell % when overall holding is %', NEW.quantity, POSITION_QUANTITY;
+        END IF;
+
+        IF TAX_BRACKET <> 0 THEN
+            QUANTITY_CNT := NEW.quantity;
+            TAX_BASE_COMP := 0;
+
+            FOR lot IN SELECT id, quantity, price, tax_base_amount 
+                        FROM TAX_LOT 
+                        WHERE asset_id = NEW.asset_id 
+                        AND portfolio_id = NEW.portfolio_id
+                        AND currency = NEW.currency
+                        ORDER BY DATE ASC  -- FIFO approach
+            LOOP
+                IF QUANTITY_CNT >= lot.quantity THEN
+                    TAX_BASE_COMP := TAX_BASE_COMP + lot.tax_base_amount;
+                    QUANTITY_CNT := QUANTITY_CNT - lot.quantity;
+                ELSE
+                    -- final, partial tax lot 
+                    TAX_BASE_COMP := TAX_BASE_COMP + ROUND(QUANTITY_CNT * lot.price, 2);
+                    EXIT;
+                END IF;
+            END LOOP;
+
+        NEW.tax_amount := ROUND(((NEW.price * NEW.quantity)-TAX_BASE_COMP) * (TAX_BRACKET / 100));
         END IF;
     END IF;
 
@@ -47,7 +91,7 @@ DECLARE
     TRANSACTION_TYPE_CODE transaction_type.code%type;
     ASSET_TYPE_CODE asset_type.code%type;
 BEGIN
-    
+    TRANSACTION_TYPE_CODE = get_transaction_type(NEW.transaction_type_id);
     SELECT code INTO TRANSACTION_TYPE_CODE 
     FROM TRANSACTION_TYPE 
     WHERE ID = NEW.transaction_type_id;
@@ -58,69 +102,74 @@ BEGIN
                 WHERE id = NEW.asset_id);
 
     -- @TODO: add booking matrix table to replace giant case here
-    RETURN NEW;
+    RETURN NULL;
 END;
 $$;
-
-CREATE OR REPLACE FUNCTION fnc_calc_position()
-RETURNS TRIGGER
-LANGUAGE plpgsql AS $$
-BEGIN
-
-    WITH trans AS (
-        SELECT 
-            sum(CASE WHEN tt.code = 'SEL' THEN (-1) * t.quantity ELSE t.quantity END) AS sum_q, 
-            sum(CASE WHEN tt.code = 'SEL' THEN (-1) * t.total_amount ELSE t.total_amount END) AS sum_t, 
-            sum(t.fee) AS sum_f, 
-            avg(CASE WHEN tt.code = 'BUY' THEN t.price END)::NUMERIC(20,4) AS avg_p, 
-            count(1) AS cnt,
-            NEW.asset_id AS asset_id,
-            NEW.portfolio_id AS portfolio_id,
-            NEW.currency AS currency
-        FROM TRANSACTION t
-        INNER JOIN TRANSACTION_TYPE tt
-        ON t.transaction_type_id = tt.id
-        WHERE 
-            t.portfolio_id = NEW.portfolio_id
-        AND t.asset_id = NEW.asset_id
-        AND t.currency = NEW.currency
-        AND tt.code IN ('BUY', 'SEL')
-    )
-    MERGE INTO POSITION p
-    USING trans t 
-    ON t.asset_id = p.asset_id
-    AND t.portfolio_id = p.portfolio_id
-    WHEN NOT MATCHED THEN
-        INSERT (portfolio_id, asset_id, currency, quantity, average_price, fee, total_amount)
-        VALUES (t.portfolio_id, t.asset_id, t.currency, t.sum_q, t.avg_p, t.sum_f, t.sum_t)
-    WHEN MATCHED AND t.cnt = 1 THEN
-        UPDATE SET 
-            quantity = t.sum_q, 
-            average_price = t.avg_p, 
-            fee = t.sum_f, 
-            total_amount = t.sum_t
-    WHEN MATCHED AND t.cnt > 1 AND t.sum_q = 0 THEN
-        DELETE 
-    WHEN MATCHED AND t.cnt > 1 AND t.sum_q > 0 THEN
-        UPDATE SET 
-            quantity = t.sum_q, 
-            average_price = t.avg_p, 
-            fee = t.sum_f, 
-            total_amount = t.sum_t;
-
-    RETURN NEW;
-END;
-$$;
-
 
 CREATE OR REPLACE FUNCTION fnc_calc_tax_lot()
 RETURNS TRIGGER 
 LANGUAGE plpgsql AS $$
+DECLARE
+    TAX_BRACKET tax_rate.rate%type;
+    TRANSACTION_TYPE_CODE transaction_type.code%type;
+    QUANTITY_CNT INTEGER;
+    TAX_BASE_COMP FINANCIAL := 0;
+    lot RECORD;
 BEGIN
-    -- @TODO: add tax lot logic here
-    -- update the transaction tax amount in case of selling based on the tax lots
     -- tax on DIV and INT calculated directly in transaction trigger
-    RETURN NEW;
+    TAX_BRACKET = get_tax_bracket(NEW.portfolio_id);
+    TRANSACTION_TYPE_CODE = get_transaction_type(NEW.transaction_type_id);
+
+    IF TRANSACTION_TYPE_CODE = 'BUY' AND TAX_BRACKET <> 0 THEN
+        INSERT INTO TAX_LOT(
+         asset_id, 
+         portfolio_id, 
+         transaction_id,
+         date, 
+         currency, 
+         quantity, 
+         price, 
+         tax_base_amount)
+        VALUES (
+         NEW.asset_id, 
+         NEW.portfolio_id,
+         NEW.id,
+         NEW.date, 
+         NEW.currency, 
+         NEW.quantity, 
+         NEW.price, 
+         NEW.quantity * NEW.price);
+
+    ELSIF TRANSACTION_TYPE_CODE = 'SEL' AND TAX_BRACKET <> 0 THEN
+        QUANTITY_CNT = NEW.quantity;
+
+        FOR lot IN SELECT id, quantity, price, tax_base_amount 
+                   FROM TAX_LOT 
+                   WHERE asset_id = NEW.asset_id 
+                   AND portfolio_id = NEW.portfolio_id
+                   AND currency = NEW.currency
+                   ORDER BY DATE ASC  -- FIFO approach
+        LOOP
+            IF QUANTITY_CNT >= lot.quantity THEN
+                TAX_BASE_COMP := TAX_BASE_COMP + lot.tax_base_amount;
+                QUANTITY_CNT := QUANTITY_CNT - lot.quantity;
+                DELETE FROM TAX_LOT WHERE id = lot.id;
+            ELSE
+                -- final, partial tax lot 
+                TAX_BASE_COMP := TAX_BASE_COMP + ROUND(QUANTITY_CNT * lot.price, 2);
+                UPDATE TAX_LOT SET 
+                quantity = lot.quantity - QUANTITY_CNT,
+                tax_base_amount = ROUND((lot.quantity - QUANTITY_CNT) * lot.price ,2)
+                WHERE id = lot.id;
+
+            EXIT;
+            END IF;
+
+        END LOOP;
+
+    END IF;
+
+    RETURN NULL;
 END;
 $$;
 
@@ -135,13 +184,7 @@ AFTER INSERT OR UPDATE ON TRANSACTION
 FOR EACH ROW
 EXECUTE FUNCTION fnc_create_booking();
 
-CREATE OR REPLACE TRIGGER trg_calc_position
-AFTER INSERT OR UPDATE ON TRANSACTION
-FOR EACH ROW
-EXECUTE FUNCTION fnc_calc_position();
-
-
-CREATE OR REPLACE TRIGGER trg_calc_tax_lot()
+CREATE OR REPLACE TRIGGER trg_calc_tax_lot
 AFTER INSERT OR UPDATE ON TRANSACTION
 FOR EACH ROW
 EXECUTE FUNCTION fnc_calc_tax_lot();
